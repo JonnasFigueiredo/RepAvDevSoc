2d96703c2abeac8007a593758d3da894c21afc3f
JonnasFigueiredo <125215987+JonnasFigueiredo@users.noreply.github.com>
2024-09-03 22:47:07 -0300

    INICIO CRUD DE FICHA INCLUSÃO DE PRIMARY KEY (INDICE)

--- src/main/resources/CRIA_TABELAS_E_INSERE_REGISTROS_INICIAIS.sql
+++ src/main/resources/CRIA_TABELAS_E_INSERE_REGISTROS_INICIAIS.sql
@@ -1,7 +1,8 @@
+
 --Exames
-CREATE TABLE exame (rowid bigint auto_increment, nm_exame VARCHAR(255));
+CREATE TABLE exame (rowid bigint auto_increment, nm_exame VARCHAR(255), PRIMARY KEY (rowid)); 
 INSERT INTO exame (nm_exame) VALUES ('Acuidade Visual'), ('Urina'), ('Clinico'), ('Toxicologico');
 
 -- Funcionarios
-CREATE TABLE funcionario (rowid BIGINT AUTO_INCREMENT, nm_funcionario VARCHAR(255));
+CREATE TABLE funcionario (rowid bigint auto_increment, nm_funcionario VARCHAR(255), PRIMARY KEY (rowid)); 
 INSERT INTO funcionario (nm_funcionario) VALUES ('Alfredo'), ('Celso'), ('Dilma');
