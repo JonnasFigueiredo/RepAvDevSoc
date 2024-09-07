c69f5ed6833b4bb843c71c1af5a35144eec7410d
JonnasFigueiredo <125215987+JonnasFigueiredo@users.noreply.github.com>
2024-09-07 18:20:10 -0300

    RELATORIO DE EXAMES

--- src/main/java/br/com/soc/sistema/infra/Action.java
+++ src/main/java/br/com/soc/sistema/infra/Action.java
@@ -8,4 +8,6 @@ public class Action extends ActionSupport {
 	/* Strings para serem usadas como Returns customizados nas Actions */
 	public static final String REDIRECT = "redirect";
 	public static final String EDIT = "edit";
+	public static final String REPORT = "report_examesrealizados";
+
 }
